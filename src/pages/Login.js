2ead3529f288370ee21b599133887732bf54e658
Benji Miles <admin@codingwithbenji.com>
2023-08-20 20:35:49 +0200

    Added content to about page

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -13,22 +13,21 @@ const Login = () => {
         <img src="https://images.unsplash.com/photo-1579871494447-9811cf80d66c?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1470&q=80" className="w-full h-full object-cover absolute -z-10"></img>
         
         <div className="flex justify-center items-center h-full">
-            <form className="max-w-[400px] w-full mx-auto p-8 border">
-                <h2 className="text-4xl font-bold text-center py-8 relative text-white">Sushi Muchi Login</h2>
-                <div className="flex flex-col mb-4">
-                    <label className="relative mb-2 text-white">Username:</label>
-                    <input className="border relative bg-gray-100 p-2" type="text" placeholder="Username"></input>
-                </div>
-                <div className="flex flex-col">
-                    <label className="relative mb-2 text-white">Password:</label>
-                    <input className="border relative bg-gray-100 p-2" type="password" placeholder="password"></input>
-                </div>
-                <button className="w-full py-3 mt-8 hover:bg-black duration-300 border relative text-white hover:text-white cursor-pointer">Sign in</button>
-                <p className="relative text-white"><input className="mr-2 relative" type="checkbox"></input>Remember this account</p>
-                <p className="relative text-white"><span className="text-blue-500 relative cursor-pointer"><Link href="/Signup">Sign up</Link></span> to become a member</p>    
-            </form>
+          <form className="max-w-[400px] w-full mx-auto p-8 border">
+            <h2 className="text-4xl font-bold text-center py-8 relative text-white">Sushi Muchi Login</h2>
+            <div className="flex flex-col mb-4">
+              <label className="relative mb-2 text-white">Username:</label>
+              <input className="border relative bg-gray-100 p-2" type="text" placeholder="Username"></input>
+            </div>
+            <div className="flex flex-col">
+              <label className="relative mb-2 text-white">Password:</label>
+              <input className="border relative bg-gray-100 p-2" type="password" placeholder="password"></input>
+            </div>
+            <button className="w-full py-3 mt-8 hover:bg-black duration-300 border relative text-white hover:text-white cursor-pointer">Sign in</button>
+            <p className="relative text-white"><input className="mr-2 relative" type="checkbox"></input>Remember this account</p>
+            <p className="relative text-white"><span className="text-blue-500 relative cursor-pointer"><Link href="/Signup">Sign up</Link></span> to become a member</p>    
+          </form>
         </div>     
-
       </div>
       <Footer/>
     </div>
